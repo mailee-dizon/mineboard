4aed491146d20d2a32174028c4006729df24cba3
Julianna Alderete <jyaldere@uci.edu>
2026-01-07 18:05:05 -0800

    styling createPost + uploadImages

--- frontend/src/components/CreatePost/UploadImages.jsx
+++ frontend/src/components/CreatePost/UploadImages.jsx
@@ -1,8 +1,10 @@
 'use client';
 import React, { useState } from 'react'
-import { Upload, ArrowLeft, ArrowRight, Plus } from 'lucide-react'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 import styles from "./UploadImages.module.css";
-import { UploadButton } from '@uploadthing/react';
+
+import { UploadDropzone } from '@uploadthing/react';
+import "@uploadthing/react/styles.css";
 
 /*
     Need to style more; when we add pic i want it to be displayed/replace uploadImagesBox
@@ -20,13 +22,17 @@ export const UploadImages = ({images, setImages}) => {
     const showNext = () => {
         setCurrentIndex(prev => (prev === images.length-1 ? 0 : prev+1));
     };
+    const removeCurrent = () => {
+
+    }
 
   return (
-        <div className={styles.upLoadImages}>
+    <div className={styles.uploadImages}>
         {images.length === 0 ?(
             <div className={styles.uploadImagesBox}>
-                <UploadButton // from uploadthing
+                <UploadDropzone // from uploadthing
                     endpoint="imageUploader"
+                    multiple
                     onClientUploadComplete={(res) =>{
                         const newImages = res.map(file => ({ 
                             url: file.ufsUrl,
@@ -36,56 +42,52 @@ export const UploadImages = ({images, setImages}) => {
                         setCurrentIndex(0);
                     }}
                     appearance={{
-                        button: { width: '100%', height: '100%', background: 'transparent', color: 'inherit' },
-                        allowedContent: { display: 'none' }                   
-                    }}
-                    content={{
-                        button: (
-                                <div className={styles.uploadImagesInner}>
-                                    <Upload className={styles.uploadIcon} />
-                                    <p className={styles.uploadImagesText}>Choose a file or drag and drop here</p>
-                                </div>
-                            )
+                        container: {border: 'none'},
+                        button: { width: '100%', height: '100%', background: 'black', color: 'white', font: 'inherit'},
                     }}
                 /> 
             </div>
         ) : ( // more than one img uploaded
-            <div className={styles.uploadImagesBox}>
-                {images.length > 1 && (
-                    <ArrowLeft 
-                        onClick={showPrev}    
-                    />
-                )}   
-                <img
-                    src={images[currentIndex].preview}
-                    alt="preview"
-                    className={styles.previewImage}
-                />        
-                {images.length > 1 && (
-                    <ArrowRight 
-                        onClick={showNext}    
-                    />
-                )}   
-                <div className={styles.addMoreSmall}>
-                    <UploadButton
-                        endpoint="imageUploader"
-                        onClientUploadComplete={(res) => {
-                            const newImages = res.map(file => ({
-                                url: file.ufsUrl,
-                                preview: file.ufsUrl
-                            }));
-                            setImages(prev => [...prev, ...newImages]);
-                        }}
-                        content={{ button: <Plus size={16} /> }}
-                        appearance={{
-                            button: { borderRadius: '50%', width: '30px', height: '30px' },
-                            allowedContent: { display: 'none' }
-                        }}
-                    />
+            <>
+            <div className={`${styles.uploadImagesBox} ${styles.deleteUploads}`}>
+                <div className={styles.multipleUploads}>
+                    {images.length > 1 && (
+                        <ArrowLeft 
+                            onClick={showPrev}    
+                        />
+                    )}
+                    <img
+                        src={images[currentIndex].preview}
+                        alt="preview"
+                        className={styles.previewImage}
+                    />        
+                    {images.length > 1 && (
+                        <ArrowRight 
+                            onClick={showNext}    
+                        />
+                    )}
                 </div>
+                <button onClick={removeCurrent} className={styles.deleteButton}>Delete Image</button>   
             </div>
+            <div className={styles.uploadImagesBox}>
+                <UploadDropzone
+                    endpoint="imageUploader"
+                    onClientUploadComplete={(res) => {
+                        const newImages = res.map(file => ({
+                            url: file.ufsUrl,
+                            preview: file.ufsUrl
+                        }));
+                        setImages(prev => [...prev, ...newImages]);
+                    }}
+                    appearance={{
+                        button: { width: '100%', height: '100%', background: 'black', color: 'white', font: 'inherit'},
+                        allowedContent: { display: 'none' }
+                    }}
+                />
+            </div>
+            </>
             )
         }
-        </div>
+    </div>
     )
 }

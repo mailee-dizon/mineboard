c0e8d544bc196d2421711db46aad8869339df691
Julianna Alderete <jyaldere@uci.edu>
2026-01-07 16:32:26 -0800

    syncs clerk users to backend

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -4,15 +4,15 @@ import { AuthProvider } from "@/context/AuthContext";
 import { ClerkProvider } from "@clerk/nextjs";
 import { minecraftFont } from "./fonts";
 import "./globals.css";
+import { UserSync } from "@/components/UserSync/UserSync";
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en" className={minecraftFont.variable}>
         <body className="font-minecraft">
-          <AuthProvider>
+          <UserSync/>
             {children}
-          </AuthProvider>
         </body>
       </html>
     </ClerkProvider>
---
+++ frontend/src/components/UserSync/UserSync.jsx
@@ -0,0 +1,38 @@
+// UserSync.jsx
+'use client';
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+// syncing current user data from DB 
+
+export const UserSync = () => {
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    useEffect(() => {
+        const handleSync = async () => {
+            if (isLoaded && isSignedIn && user) {
+                try {
+                    const payload = {
+                        userId: user.id,
+                        username: user.username || user.firstName || "Anonymous",
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        pfp: user.imageUrl
+                    };
+
+                    await fetch(`https://mineboard.onrender.com/api/users`, {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify(payload)
+                    });                    
+                } catch (err) {
+                    console.error("Sync error:", err);
+                }
+            }
+        };
+
+        handleSync();
+    }, [isLoaded, isSignedIn, user]);
+
+    return null; 
+};

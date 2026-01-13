504fd7cc62f2184790d37bc7bad186d678b979b1
Julianna Alderete <jyaldere@uci.edu>
2026-01-13 13:41:50 -0800

    fixed merge conflict

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,9 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
-=======
-import { getAnalytics } from "firebase/analytics";
->>>>>>> main
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,8 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
-=======
-const analytics = getAnalytics(app);
->>>>>>> main
 export const storage = getStorage(app);

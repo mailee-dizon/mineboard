a8ad8f15d2d3633692ca3d7285147c9ff643ae6c
Mailee Mendoza Dizon <maileed@uci.edu>
2026-01-16 13:04:06 -0800

    adding profile header

--- frontend/src/app/(main)/profileview/[id]/page.jsx
+++ frontend/src/app/(main)/profileview/[id]/page.jsx
@@ -1,12 +1,8 @@
 import ProfileHeader from "@/components/ProfileHeader/ProfileHeader";
-<<<<<<< HEAD
 import { API_URL } from "../../../../../constants/api.js";
 import ProfileContent from "@/components/ProfileContent/ProfileContent.jsx";
-=======
 import React from "react";
-import { API_URL } from "../../../../../constants/api.js";
 import { PostFeed } from "@/components/Post/PostFeed.jsx";
->>>>>>> 97065bd2f9848ba18a0c91e68be15d673d8dc59b
 
 export default async function ProfilePage({ params }) {
     const { id } = await params;
@@ -20,11 +16,8 @@ export default async function ProfilePage({ params }) {
     return (
         <div>
             <ProfileHeader profileUser={id} initialData={data[0]}/>
-<<<<<<< HEAD
             <ProfileContent/>
-=======
             <PostFeed initialPosts={userPosts}/>
->>>>>>> 97065bd2f9848ba18a0c91e68be15d673d8dc59b
         </div>
     )
 }

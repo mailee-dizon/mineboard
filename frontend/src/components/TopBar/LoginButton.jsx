d94f531768dee53d487e61b461a545f883d35a4d
Mailee Mendoza Dizon <maileed@uci.edu>
2026-01-16 22:58:51 -0800

    fixing top bar login/pfp button

--- frontend/src/components/TopBar/LoginButton.jsx
+++ frontend/src/components/TopBar/LoginButton.jsx
@@ -5,13 +5,13 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-export const LoginButton = ({ initialData }) => {
-    const { isSignedIn, user } = useUser();
+export const LoginButton = ({ initialData, isLogged }) => {
+    const { user } = useUser();
     const userId = user?.id;
     const pfp = initialData?.pfp
     const router = useRouter()
 
-    if (!isSignedIn || !user){
+    if (!isLogged){
         return (
             <SignInButton mode="modal">
                 <button className={styles.loginButton}>Login</button>
@@ -22,7 +22,7 @@ export const LoginButton = ({ initialData }) => {
  
 
     return(
-        <button className={styles.loginButton} onClick={() => router.push(`/profileview/${userId}`)}>
+        <button className={styles.pfpButton} onClick={() => router.push(`/profileview/${userId}`)}>
             {pfp ? (
                 <div >
                     <Image

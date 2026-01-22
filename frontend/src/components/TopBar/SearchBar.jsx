8eba6936b1ebfd9ebce89e95be328fb81c917fdf
Julianna Alderete <jyaldere@uci.edu>
2026-01-21 18:24:07 -0800

    prototype search results (category/title)

--- frontend/src/components/TopBar/SearchBar.jsx
+++ frontend/src/components/TopBar/SearchBar.jsx
@@ -1,22 +1,42 @@
 import React, {use, useState} from "react";
 import { Search } from "lucide-react";
 import styles from "./SearchBar.module.css";
+import { API_URL } from '../../../constants/api';
 
 export const SearchBar = ({setResults}) => {
 
     const [input, setInput] = useState("")
    
+    /* we want to be able to search:
+        1. users (usernames) -> display users
+        2. categories 
+        3. by title (should not be exact. if u search "cottagecore" any titles with cottagecore or cottage within it should pop up)
+    */
+
     /* Placeholder for fetching data for search results. Once we figure out backend and connect, this will change */
-    const fetchData = (value) => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((json) => {
-            const results = json.filter((user) => {
-                return value && user && user.name && user.name.toLowerCase().includes(value);
-            });
-            setResults(results);
-        });
-    }
+    const fetchData = async (value) => {
+        const title_results = await fetch(`${API_URL}/posts/title/${value}`); // getPostsByTitle
+        const title_data = await title_results.json();
+
+        const category_results = await fetch(`${API_URL}/posts/category`, 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    categories: [value]
+                })
+
+            }); // getPostsByCategory
+        const category_data = await category_results.json();
+
+        const results = [...title_data, ...category_data];
+        console.log("results", results)
+
+        setResults(results);
+        };
+
     const handleChange = (value) => {
         setInput(value)
         fetchData(value)
--- frontend/src/components/TopBar/SearchResults.jsx
+++ frontend/src/components/TopBar/SearchResults.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import styles from "./SearchResults.module.css";
 import { useRouter } from "next/navigation";
 
-
 export const SearchResults = ({ results }) => {
     const router = useRouter()
-
+    
     return <div className={styles["results-list"]}>
     {
-        results.map((result, id) =>{
-            return (
-            <div>
-                <button className={styles.result} key={id} onClick={()=> {router.push(`/home`)}}>
-                    {result.name}
+        Object.values(results).map((result) => (
+            <div className={styles.resultContainer}>
+                <button 
+                    className={styles.resultOption} 
+                    key={result.postid} 
+                    onClick={()=> {router.push(`/postview/${result.userid}/${result.postid}`)}}
+                >
+                    {result.title}
                 </button>
 
             </div>
-            )
-        })
+        
+        ))
     }
 
     </div>

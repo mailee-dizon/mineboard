26e44f8972969f34bb11087051a1c341c6058167
Mailee Mendoza Dizon <maileed@uci.edu>
2026-02-12 17:51:01 -0800

    edit routes

--- backend/routes/likesRoutes.js
+++ backend/routes/likesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { likePost, unlikePost } from "../controllers/likesController.js";
+import { likePost, unlikePost, getUserLike } from "../controllers/likesController.js";
 const router = express.Router();
 
 router.post("/:userId/:postId", likePost);
 router.delete("/:userId/:postId", unlikePost);
+router.get("/:userId/:postId", getUserLike);
 
 export default router;
